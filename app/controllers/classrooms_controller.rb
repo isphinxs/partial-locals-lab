5bab8094ac452d2826e5b227103651e0ecfa289f
Samantha Ostrowski <isphinxs@hotmail.com>
2021-04-10 20:32:23 -0400

    commit tree dcba2cca4176f578c689750c503cf8dd55ab4b6c

--- app/controllers/classrooms_controller.rb
+++ app/controllers/classrooms_controller.rb
@@ -1,7 +1,8 @@
 class ClassroomsController < ApplicationController
   def show
     @classroom = Classroom.find(params[:id])
-    @student = @classroom.oldest_student
+    @students = Student.search(params[:query])
+    # byebug
   end
 
   def index
